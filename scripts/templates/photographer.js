26d87d76c2efd66868ba043ee1f052664e3c3694
SALM Loïc <loic.salm8@gmail.com>
2023-11-16 09:58:33 +0100

    add styles index,tab index

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,6 +5,10 @@ function photographerTemplate(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        
+        const headerDiv = document.createElement('div');
+        headerDiv.className = 'photographer-header';
+        
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
@@ -13,16 +17,27 @@ function photographerTemplate(data) {
         article.appendChild(h2);
         const pCity = document.createElement('p');
         pCity.textContent = `${city}, ${country}`;
+        pCity.classList.add("country-style");
         const pTagline = document.createElement('p');
         pTagline.textContent = tagline;
+        pTagline.classList.add("tagline-style");
         const pPrice = document.createElement('p');
         pPrice.textContent = `${price}€/jour`;
+        pPrice.classList.add("price-style");
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(pCity);
-        article.appendChild(pTagline);
-        article.appendChild(pPrice);
+        const infoDiv = document.createElement('div');
+        infoDiv.className = 'photographer-info';
+
+        headerDiv.appendChild(img);
+        headerDiv.appendChild(h2);
+        article.appendChild(headerDiv);
+        infoDiv.appendChild(pCity);
+        infoDiv.appendChild(pTagline);
+        infoDiv.appendChild(pPrice);
+        article.appendChild(infoDiv);
+
+        headerDiv.tabIndex = 0; // Permettre la navigation au clavier
+        infoDiv.tabIndex = 0; 
 
         return (article);
     }
